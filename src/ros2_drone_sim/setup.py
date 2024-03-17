1df1ae87eafb8eb03dead4c8c8c2a4e9ce9282d1
kingsoftech <asoyero@student.lautech.edu.ng>
2024-03-17 08:34:39 +0100

    file update

---
+++ src/ros2_drone_sim/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'ros2_drone_sim'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name,'worlds'), glob('worlds/*')),
+        (os.path.join('share', package_name,'models'), glob('models/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='abdullah',
+    maintainer_email='asoyero@student.lautech.edu.ng',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
